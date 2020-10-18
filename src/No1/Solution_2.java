fab3130ef8d5db4ca40c0973dd3064d6645dd38c
HRR <1160884290@qq.com>
2020-10-19 00:11:08 +0800

    与右上角或左下角对比法

---
+++ src/No1/Solution_2.java
@@ -0,0 +1,28 @@
+package No1;
+public class Solution_2 {
+    public boolean Find(int target, int [][] array) {
+        //排除空数组
+        if(array.length==1&&array[0].length==0||array.length==0)
+            return false;
+
+        /*
+        选取右上角元素与target比较，
+        如果比target小则可能在左侧，选取元素列减1，j--
+        如果大，可能在下侧 行减1  i--
+        */
+        for(int j=array[0].length-1,i=0;j>=0&&i<array.length;)
+        {
+            if(array[i][j]==target)
+                return true;
+            else if(array[i][j]>target)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return false;
+    }
+}
